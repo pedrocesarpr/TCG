1c482fdf3ea6afef0c9618a3246585d450e26f31
Pedro Cesar <Pedro Cesar@DROGON>
2025-08-27 12:04:57 -0300

    Healthcheck

---
+++ PokemonTCGOrganizerApi/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PokemonTCGOrganizer.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet("ping")]
+        public IActionResult Ping()
+        {
+            return Ok(new { status = "pong" });
+        }
+    }
+}
